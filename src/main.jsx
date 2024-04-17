6d83f49ae652ae1f4c5b88fa55c49f32bc70bd0f
Daniel Ntege <danientege785@gmail.com>
2024-04-17 08:39:16 +0300

    commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://6c1caad7917fabdd4e5113e97bf9ffd9@o4507097814401024.ingest.us.sentry.io/4507097830653952",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
